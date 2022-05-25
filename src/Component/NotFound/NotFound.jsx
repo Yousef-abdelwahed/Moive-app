237267ac1795421d242f5f70e393648fff2c630e
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2022-05-25 11:45:58 +0200

    Create NotFound.jsx

---
+++ src/Component/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Notfound from './notfound.jfif'
+export default function NotFound() {
+  return (
+    <> 
+    <div className='text-center'>
+    <img src={Notfound} alt="" />
+    </div>
+    </>
+  )
+}
